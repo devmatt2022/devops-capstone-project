d206c20b522608d9c727b244841eccda069ac2a0
Your Name <you@example.com>
2025-12-31 16:11:48 -0500

    Added CORS headers

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -227,6 +227,13 @@ class TestAccountService(TestCase):
         for key, value in headers.items():
             self.assertEqual(response.headers.get(key), value)
 
+    def test_cors_security(self):
+        """It should return a CORS header"""
+        response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # Check for the CORS header
+        self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
+
 """
 read-accounts.jpeg (or read-accounts.png) — for the story “Read an account from the service.”
 list-accounts.jpeg (or list-accounts.png) — for the story “List all accounts in the service.”
